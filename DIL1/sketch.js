db20fa255f62b58bdeddb26489bfba2a938ea6d2
AustinBrownProjects <austin.js.brown@gmail.com>
2025-01-21 19:02:55 +0000

    added sprite

--- DIL1/sketch.js
+++ DIL1/sketch.js
@@ -1,18 +1,16 @@
-let ball;
-
 function setup() {
 	new Canvas(500, 500);
-	displayMode('centered');
+	world.gravity.y= 10;
 
-	ball = new Sprite();
-	ball.diameter = 50;
+	square = new Sprite(100,100,50,50,'d')
+
+	rectangle = new Sprite(50,50,100,10,'d')
+
+	circle = new Sprite(50,50,10,'d')
+	
 }
 
 function draw() {
 	background('skyblue');
 
-	if (mouse.presses()) {
-		ball.speed = 10;
-		ball.moveTo(mouse);
-	}
 }
