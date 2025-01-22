ef56c4a92fee75945c56e975eda77ba42c0a7cc8
AustinBrownProjects <austin.js.brown@gmail.com>
2025-01-22 08:19:30 +0000

    Added towers, improved jump physics

--- DIL1/sketch.js
+++ DIL1/sketch.js
@@ -1,16 +1,44 @@
+let Rhys;
+
 function setup() {
-	new Canvas(500, 500);
+	new Canvas(1200, 500);
 	world.gravity.y= 10;
 
-	square = new Sprite(100,100,50,50,'d')
+	 	Rhys = new Sprite(10,250,'d');
+		 Rhys.vel.x = 2
+			Rhys.diameter = 20;
 
-	rectangle = new Sprite(50,50,100,10,'d')
 
-	circle = new Sprite(50,50,10,'d')
-	
+	PillarOne = new Sprite(325,height-125,75,250,'s');
+	PillarTwo = new Sprite(325,25,75,100,'s')
+
+	PillarThree = new Sprite(525,160,75,320,'s');
+	PillarFour = new Sprite(525,height-25,75,50,'s')
+
+	PillarFive = new Sprite(725,145,75,290,'s');
+	PillarSix = new Sprite(725,height-25,75,50,'s')
+
+	PillarSeven = new Sprite(925,160,75,320,'s');
+	PillarEight = new Sprite(925,height-25,75,50,'s')
+
+	PillarNine = new Sprite(525,160,75,320,'s');
+	PillarTen = new Sprite(525,height-25,75,50,'s')
 }
 
 function draw() {
-	background('skyblue');
+ background('skyblue');
+
+
+
+}
+
+function keyPressed() {
+	if ( key = ' '){
+	Rhys.y -= 75;
+	Rhys.vel.y = 0
+
+	}
+
+
 
 }
