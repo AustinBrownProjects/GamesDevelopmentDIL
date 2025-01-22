39b457497afc36c64f13d3164622c2952be30c4a
AustinBrownProjects <austin.js.brown@gmail.com>
2025-01-22 13:07:43 +0000

    Added towers, improved jump physics

--- DIL1/sketch.js
+++ DIL1/sketch.js
@@ -10,19 +10,23 @@ function setup() {
 
 
 	PillarOne = new Sprite(325,height-125,75,250,'s');
-	PillarTwo = new Sprite(325,25,75,100,'s')
+		PillarOne.colour = 'lime'
+
+	PillarTwo = new Sprite(325,height-125,75,250,'s');
+		PillarTwo.colour = 'lime'
 
 	PillarThree = new Sprite(525,160,75,320,'s');
+
 	PillarFour = new Sprite(525,height-25,75,50,'s')
 
 	PillarFive = new Sprite(725,145,75,290,'s');
-	PillarSix = new Sprite(725,height-25,75,50,'s')
+	PillarSix = new Sprite(725,height-50,75,100,'s')
 
-	PillarSeven = new Sprite(925,160,75,320,'s');
-	PillarEight = new Sprite(925,height-25,75,50,'s')
+	PillarSeven = new Sprite(925,40,75,80,'s');
+	PillarEight = new Sprite(925,height-160,75,350,'s')
 
-	PillarNine = new Sprite(525,160,75,320,'s');
-	PillarTen = new Sprite(525,height-25,75,50,'s')
+	PillarNine = new Sprite(1125,105,75,210,'s');
+	PillarTen = new Sprite(1125,height-105,75,210,'s')
 }
 
 function draw() {
@@ -34,8 +38,8 @@ function draw() {
 
 function keyPressed() {
 	if ( key = ' '){
-	Rhys.y -= 75;
-	Rhys.vel.y = 0
+	Rhys.y -= 50;
+	Rhys.vel.y = 0;
 
 	}
 
