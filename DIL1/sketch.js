dd6d493c925f29a10c24d62dd3b062c88cd67acb
AustinBrownProjects <austin.js.brown@gmail.com>
2025-01-22 13:44:53 +0000

    grouped towers, added collison

--- DIL1/sketch.js
+++ DIL1/sketch.js
@@ -5,31 +5,46 @@ function setup() {
 	world.gravity.y= 10;
 
 	 	Rhys = new Sprite(10,250,'d');
-		 Rhys.vel.x = 2
+
+
 			Rhys.diameter = 20;
 
+allPillars = new Group()
 
-	PillarOne = new Sprite(325,height-125,75,250,'s');
-		PillarOne.colour = 'lime'
-	PillarTwo = new Sprite(325,height-125,75,250,'s');
-		PillarTwo.colour = 'lime'
+	PillarOne = new allPillars.Sprite(325,75,75,150,'s');
+		PillarOne.colour = 'lime';
+	PillarTwo = new allPillars.Sprite(325,height-125,75,250,'s');
+		PillarTwo.colour = 'lime';
 
-	PillarThree = new Sprite(525,160,75,320,'s');
+	PillarThree = new allPillars.Sprite(525,160,75,320,'s');
+		PillarThree.colour = 'lime';
+	PillarFour = new allPillars.Sprite(525,height-25,75,50,'s')
+		PillarFour.colour = 'lime';
 
-	PillarFour = new Sprite(525,height-25,75,50,'s')
+	PillarFive = new allPillars.Sprite(725,145,75,290,'s');
+		PillarFive.colour = 'lime';
+	PillarSix = new allPillars.Sprite(725,height-50,75,100,'s')
+		PillarSix.colour = 'lime';
 
-	PillarFive = new Sprite(725,145,75,290,'s');
-	PillarSix = new Sprite(725,height-50,75,100,'s')
+	PillarSeven = new allPillars.Sprite(925,40,75,80,'s');
+		PillarSeven.colour = 'lime';
+	PillarEight = new allPillars.Sprite(925,height-140,75,310,'s')
+		PillarEight.colour = 'lime';
 
-	PillarSeven = new Sprite(925,40,75,80,'s');
-	PillarEight = new Sprite(925,height-160,75,350,'s')
+	PillarNine = new allPillars.Sprite(1125,105,75,210,'s');
+		PillarNine.colour = 'lime';
+	PillarTen = new allPillars.Sprite(1125,height-105,75,210,'s')
+		PillarTen.colour = 'lime';
 
-	PillarNine = new Sprite(1125,105,75,210,'s');
-	PillarTen = new Sprite(1125,height-105,75,210,'s')
+	Rhys.collides(allPillars,reset)
 }
-
+function reset(){
+	Rhys.x = 50;
+	Rhys.y  = 50;
+}
 function draw() {
  background('skyblue');
+	Rhys.vel.x = 2
 
 
 
@@ -37,8 +52,7 @@ function draw() {
 
 function keyPressed() {
 	if ( key = ' '){
-	Rhys.y -= 50;
-	Rhys.vel.y = 0;
+		Rhys.vel.y = -5;
 
 	}
 
