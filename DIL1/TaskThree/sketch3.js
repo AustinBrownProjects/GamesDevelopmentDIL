5392e9ae022574a0f0f3835b155c228459074940
AustinBrownProjects <austin.js.brown@gmail.com>
2025-01-23 21:32:02 +0000

    added movement to blocks

--- DIL1/TaskThree/sketch3.js
+++ DIL1/TaskThree/sketch3.js
@@ -1,7 +1,21 @@
-
+let block;
 
 function setup() {
-	new Canvas(500, 500);
+	new Canvas(600, 500);
+	wallsGroup = new Group();
+	wallsGroup.colour = 'white'
+
+	topWall = new wallsGroup.Sprite(width/2,height-height,width,10,'s');
+	bottomWall = new wallsGroup.Sprite(width/2,height,width,10,'s');
+	leftWall = new wallsGroup.Sprite(width-width,height/2,10,height,'s');
+	rightWall = new wallsGroup.Sprite(width,height/2,10,height,'s');
+
+
+		block = new Sprite(width/2-20,20,100,20,'d')
+			block.bounciness= 1;
+				block.vel.x = 3;
+					block.collides(bottomWall,stop);
+
 
 
 
@@ -13,5 +27,16 @@ function draw() {
 	background('skyblue');
 
 
+
+}
+
+function keyPressed() {
+	if ( key = ' '){
+		block.vel.x = 0;
+		block.vel.y =10
+	}
+	function stop(){
+		block.collider = 'static';
+	}
 
 }
